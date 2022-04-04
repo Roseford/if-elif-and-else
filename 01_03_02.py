2aea576a02983877a3be744cceb4f6b90189f5cc
Obianuju <obianujuofodu@gmail.com>
2022-04-04 14:27:03 +0100

    Add files via upload

---
+++ 01_03_02.py
@@ -0,0 +1,20 @@
+#if, elif, else 
+#OPERATORS: ==,!=, <=, >=, >, <
+
+# name = input('what is your name')
+# grade = input('what did you score in your exam? ')
+# if grade == 'A':
+#     print('bravo!!....Go on to the first class')
+# elif grade == 'B':
+#     print('good job!.... Go on to the second class')
+# elif grade == 'C':
+#     print('weldone ..... Go on to the third class')
+# else:
+#     print('you aren\'t allowed to the next class')        
+
+
+age = int(input('How old are you? '))
+if age >= 30:
+    print('You arent allowed in this program')
+else:
+    print('Welcome! click the link below to continue')    
